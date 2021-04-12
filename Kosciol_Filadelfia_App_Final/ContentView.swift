f15d5fd486ff521216f3887a0465fae22291ef49
Natanael Jop <natanael.jop@icloud.com>
2021-04-12 20:42:40 +0200

    Initial Commit

---
+++ Kosciol_Filadelfia_App_Final/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kosciol_Filadelfia_App_Final
+//
+//  Created by Natanael  on 12/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kosciol_Filadelfia_App_Final/Kosciol_Filadelfia_App_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kosciol_Filadelfia_App_FinalApp.swift
+//  Kosciol_Filadelfia_App_Final
+//
+//  Created by Natanael  on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Kosciol_Filadelfia_App_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
